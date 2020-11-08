9fb3696c7604d87c59cde640cde44139b233bb6e
KevinArismendy <kevinarismendy21@gmail.com>
2020-11-08 17:49:27 -0500

    Cambio de diseño Login
    Cambio de maquetacion de login.php el login anterior no se borro, se cambio el nombre

--- usuario/login/loginantiguo.php
+++ usuario/login/loginantiguo.php
@@ -9,10 +9,13 @@ if (isset($_POST['login'])) { // Esto es para que el condicional SOLO se active 
     //$query->bindParam("nameuser", $username, PDO::PARAM_STR);
     //$query->execute();
     //$result = $query->fetch(PDO::FETCH_ASSOC);
-    $sql_users = "SELECT * FROM tbl_user WHERE nameuser = ? AND contrasena = ?";
+    $sql_users = "SELECT * FROM tbl_user WHERE nameuser =:nameuser AND contrasena =:contrasena";
     $resultado_query = $pdo->prepare($sql_users);
+        
+        $resultado_query->bindparam(':nameuser', $username);
+        $resultado_query->bindparam(':contrasena', $password);
 
-        $resultado_query -> execute(array($username, $password));
+        $resultado_query -> execute();
 
         $resultado_query = $resultado_query->fetchAll(PDO:: FETCH_ASSOC);
 
@@ -21,13 +24,14 @@ if (isset($_POST['login'])) { // Esto es para que el condicional SOLO se active 
         if ($cantidad_usuarios == 1) {
             echo "<script>alert('Logeado Correctamente');</script>";
 
-            header("Status: 301 Moved Permanently");
-            header("Location: ../perfil/perfil.php");
-            exit;
-
             $id_user = $resultado_query[0]['idtbl_user'];
             $_SESSION['autorizado']=true;
             $_SESSION['idtbl_user']= $id_user;
+
+            //header("Status: 301 Moved Permanently");
+            header("Location: ../perfil/perfil.php");
+            exit;
+
             //echo $id_user;
 
         // echo '<meta http-equiv="refresh" content="1,starter.php">';
@@ -49,22 +53,24 @@ if (isset($_POST['login'])) { // Esto es para que el condicional SOLO se active 
 }
 ?>
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
-    <meta charset="utf-8">
-    <!--  This file has been downloaded from bootdey.com    @bootdey on twitter -->
-    <!--  All snippets are MIT license http://bootdey.com/license -->
-    <title>bs4 beta login - Bootdey.com</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="loginform.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=evice-width, initial-sacle=1.0">
+    <link rel="icon" type="image/png" href="../../img/logo/domperfav.png" />
     <script src="../../js/fonts/fonts.js" crossorigin="anonymous"></script>
-    <script src="../../js/bootstrap.bundle.min.js"></script>
-    <script src="../../js/jquery-3.5.1.js"></script>
-    <script type="text/javascript"></script>
+    <link href="../../css/fonts/fonts.css" rel="stylesheet" />
+    <link href="../../css/fonts/fonts1.css" rel="stylesheet" type="text/css" />
+    <link href="../../css/popup.css" rel="stylesheet" />
+    <link href="../../css/styles.css" rel="stylesheet" />
+    <link href="../../css/stylesrob.css" rel="stylesheet" />
+    <link href="../../css/regisandlog2.css" rel="stylesheet" />
+    <link href="../../css/domperstyles2.css" rel="stylesheet" /> 
+    <title>Iniciar sesión</title>
 </head>
 <body>
-  <!-- Navigation-->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container">
                 <a class="navbar-brand js-scroll-trigger" href="../../index.html">Domper</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
@@ -80,47 +86,18 @@ if (isset($_POST['login'])) { // Esto es para que el condicional SOLO se active 
                 </div>
             </div>
         </nav><br><br><br>
-<br>
-<br>
-<div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card-group mb-0">
-          <div class="card p-4">
-            <div class="card-body">
-              <h1>Login</h1>
-              <p class="text-muted">Sign In to your account</p>
-              <div class="input-group mb-3">
-                <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                <input type="text" class="form-control" placeholder="Username">
-              </div>
-              <div class="input-group mb-4">
-                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                <input type="password" class="form-control" placeholder="Password">
-              </div>
-              <div class="row">
-                <div class="col-6">
-                  <button type="button" class="btn btn-primary px-4">Login</button>
-                </div>
-                <div class="col-6 text-right">
-                  <button type="button" class="btn btn-link px-0">Forgot password?</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
-            <div class="card-body text-center">
-              <div>
-                <h2>Sign up</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <button type="button" class="btn btn-primary active mt-3">Register Now!</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+<form method="post" action="login.php" name="signin-form">
+<label id="insesionlab">Inicio sesión</label><br><br>
+    <div class="form-element">
+        <label>Usuario</label>
+        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
     </div>
-  </div>
-
+    <div class="form-element">
+        <label>Contraseña</label>
+        <input type="password" name="password" required />
+    </div>
+    <button type="submit" name="login" value="login">Acceder</button><br><br>
+    <div class="opcioncontra"><a href="">¿Olvidaste tu contraseña?</a></div>
+</form>
 </body>
 </html>
--- usuario/register/signup.php
+++ usuario/register/signup.php
@@ -6,8 +6,10 @@
     <!--  All snippets are MIT license http://bootdey.com/license -->
     <title>bs4 sign up page - Bootdey.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="http://netdna.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
-    <style type="text/css">
+    <!-- <link href="http://netdna.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet"> -->
+	<link href="../../css/bootstrap.min.css" rel="stylesheet">
+ 
+ <style type="text/css">
     	body{
     margin-top:20px;
     background-color: #f2f3f8;
@@ -33,41 +35,56 @@
     </style>
 </head>
 <body>
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+		<div class="container">
+			<a class="navbar-brand js-scroll-trigger" href="../../index.html">Domper</a>
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto my-2 my-lg-0">
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="../../index.html#about">Sobre Nosotros</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="../../index.html#services">Servicios</a></li>
+					<!--<li class="nav-item"><a class="nav-link js-scroll-trigger" href="../index.html#portfolio">Portafolio</a></li>-->
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="../../index.html#contact">Contáctanos</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="../login/login.php">Iniciar Sesion</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="reguser.php">Registrarse</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav><br><br><br>
+<form method="POST" action="" name="signin-form">
 <div class="container h-100">
     		<div class="row h-100">
 				<div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
 					<div class="d-table-cell align-middle">
-
 						<div class="text-center mt-4">
-							<h1 class="h2">Get started</h1>
+							<h1 class="h2">Registrate</h1>
 							<p class="lead">
-								Start creating the best possible user experience for you customers.
+									Empieza a usar Domper como un crack.
 							</p>
 						</div>
-
 						<div class="card">
 							<div class="card-body">
 								<div class="m-sm-4">
 									<form>
 										<div class="form-group">
-											<label>Name</label>
-											<input class="form-control form-control-lg" type="text" name="name" placeholder="Enter your name">
+											<label>Nombres</label>
+											<input class="form-control form-control-lg" type="text" name="name" placeholder="Pon Tu Nombre">
 										</div>
 										<div class="form-group">
-											<label>Company</label>
-											<input class="form-control form-control-lg" type="text" name="company" placeholder="Enter your company name">
+											<label>Apellidos</label>
+											<input class="form-control form-control-lg" type="text" name="company" placeholder="Pon tus Apellidos">
 										</div>
 										<div class="form-group">
 											<label>Email</label>
-											<input class="form-control form-control-lg" type="email" name="email" placeholder="Enter your email">
+											<input class="form-control form-control-lg" type="email" name="email" placeholder="Escribe tu e-mail">
 										</div>
 										<div class="form-group">
-											<label>Password</label>
-											<input class="form-control form-control-lg" type="password" name="password" placeholder="Enter password">
+											<label>Contraseña</label>
+											<input class="form-control form-control-lg" type="password" name="password" placeholder="Crea una contraseña">
 										</div>
 										<div class="text-center mt-3">
-											<a href="index.html" class="btn btn-lg btn-primary">Sign up</a>
-											<!-- <button type="submit" class="btn btn-lg btn-primary">Sign up</button> -->
+											<button type="submit" class="btn btn-lg btn-primary"><a href="index.html">Registrarse</a></button>
 										</div>
 									</form>
 								</div>
@@ -78,6 +95,7 @@
 				</div>
 			</div>
 		</div>
+</form>
 <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="http://netdna.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script type="text/javascript">
