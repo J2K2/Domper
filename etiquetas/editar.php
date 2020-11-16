2d0fa9ebe6fc2cca96d60126dfed8079af9e26f3
KevinArismendy <kevinarismendy21@gmail.com>
2020-11-15 19:20:10 -0500

    Creacion para editar etiquetas
    Creacion de archivos para editar las etiquetas y tambien para verlas

---
+++ etiquetas/editar.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!--  This file has been downloaded from bootdey.com    @bootdey on twitter -->
+    <!--  All snippets are MIT license http://bootdey.com/license -->
+    <title>contact details page - Bootdey.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="editar_tags_styles.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container bootstrap snippets bootdey">
+<div class="row ng-scope">
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-body text-center">
+                <!--<div class="pv-lg"><img class="center-block img-responsive img-circle img-thumbnail thumb96" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="Contact"></div>-->
+                <h3 class="m0 text-bold">Etiqueta</h3>
+                <div class="mv-lg">
+                    <p>DESCRIPCION ASDFJHKSFHJKLSADHKFLHJASDHFJ</p>
+                </div>
+                <div class="text-center"><a class="btn btn-primary" href="">Volver</a></div>
+            </div>
+        </div>
+        
+        <div class="panel panel-default hidden-xs hidden-sm">
+            <!--
+            <div class="panel-heading">
+                <div class="panel-title text-center">Recent contacts</div>
+            </div>
+            <div class="panel-body">
+                <div class="media">
+                    <div class="media-left media-middle">
+                        <a href="#"><img class="media-object img-circle img-thumbnail thumb48" src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="Contact"></a>
+                    </div>
+                    <div class="media-body pt-sm">
+                        <div class="text-bold">Floyd Ortiz
+                            <div class="text-sm text-muted">12m ago</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="media">
+                    <div class="media-left media-middle">
+                        <a href="#"><img class="media-object img-circle img-thumbnail thumb48" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="Contact"></a>
+                    </div>
+                    <div class="media-body pt-sm">
+                        <div class="text-bold">Luis Vasquez
+                            <div class="text-sm text-muted">2h ago</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="media">
+                    <div class="media-left media-middle">
+                        <a href="#"><img class="media-object img-circle img-thumbnail thumb48" src="https://bootdey.com/img/Content/avatar/avatar4.png" alt="Contact"></a>
+                    </div>
+                    <div class="media-body pt-sm">
+                        <div class="text-bold">Duane Mckinney
+                            <div class="text-sm text-muted">yesterday</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="media">
+                    <div class="media-left media-middle">
+                        <a href="#"><img class="media-object img-circle img-thumbnail thumb48" src="https://bootdey.com/img/Content/avatar/avatar5.png" alt="Contact"></a>
+                    </div>
+                    <div class="media-body pt-sm">
+                        <div class="text-bold">Connie Lambert
+                            <div class="text-sm text-muted">2 weeks ago</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        -->
+        </div>
+    </div>
+    <div class="col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="h4 text-center">Information</div>
+                <div class="row pv-lg">
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-8">
+                        <form class="form-horizontal ng-pristine ng-valid">
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="inputContact1">Name</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" id="inputContact1" type="text" placeholder="" value="Audrey Hunt">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="inputContact2">Email</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" id="inputContact2" type="email" value="mail@example.com">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="inputContact3">Phone</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" id="inputContact3" type="text" value="(123) 465 789">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="inputContact4">Mobile</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" id="inputContact4" type="text" value="(12) 123 987 465">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="inputContact5">Website</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" id="inputContact5" type="text" value="http://some.wesbite.com">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="inputContact6">Address</label>
+                                <div class="col-sm-10">
+                                    <textarea class="form-control" id="inputContact6" row="4">Some nice Street, 1234</textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="inputContact7">Social</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" id="inputContact7" type="text" value="@Social">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="inputContact8">Company</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" id="inputContact8" type="text" placeholder="No Company">
+                                </div>
+                            </div>
+                            <!--<div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox"> Favorite contact?</label>
+                                    </div>
+                                </div>
+                            </div>-->
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <button class="btn btn-info" type="submit">Update</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="text-right"><a class="text-muted" href="#">Borrar</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+	
+</script>
+</body>
+</html>
